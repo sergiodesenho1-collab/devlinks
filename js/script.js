51c4aa8b52e791e8e31cbfeb330eaebe213cd2c5
sergiodesenho1-collab <sergiodesenho1@gmail.com>
2026-04-20 10:37:59 -0300

    adicionando javascript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img");
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver ligth mode,adicionar a imagem ligth
+    img.setAttribute("src", "/assets/avatar-light.png");
+  } else {
+    //se nao tiver light mode,adicionar a imagem dark
+    img.setAttribute("src", "/assets/avatar.png");
+  }
+}
